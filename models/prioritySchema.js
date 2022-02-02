23ae7b525e0923fc660e602fb318bc5e914e53c4
Utsav Shrestha <utsavshrestha@Utsavs-MacBook-Pro.local>
2022-02-02 15:55:08 +0545

    added priority schema

---
+++ models/prioritySchema.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const prioritySchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "Please enter Priority name"],
+    },
+    color: {
+        type: String,
+        required: [true, "Please select Priority color"],
+    },
+
+});
+
+const Priority = mongoose.model("Priority", prioritySchema);
+module.exports = Priority;
