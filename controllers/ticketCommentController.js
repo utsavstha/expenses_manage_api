557491d86065964265b3e08980facde2545c4fca
Utsav Shrestha <utsavshrestha@Utsavs-MacBook-Pro.local>
2022-02-14 22:15:00 +0545

    fixed get comments api with aggregate

--- controllers/ticketCommentController.js
+++ controllers/ticketCommentController.js
@@ -1,5 +1,6 @@
 const TicketComment = require("../models/ticketCommentsModel");
 const base = require("./baseController");
+const mongoose = require("mongoose");
 
 exports.deleteMe = async (req, res, next) => {
   try {
@@ -18,7 +19,25 @@ exports.deleteMe = async (req, res, next) => {
 
 exports.getTicketComments = async (req, res, next) => {
   try {
-    let doc = await TicketComment.find({ ticket: req.params.id });
+    let doc = await TicketComment.aggregate([
+      { $match: { ticket: mongoose.Types.ObjectId(req.params.id) } },
+
+      {
+        $lookup: {
+          from: "users",
+          localField: "posted_by",
+          foreignField: "_id",
+          as: "posted_by",
+        },
+      },
+      {
+        $set: {
+          posted_by: { $arrayElemAt: ["$posted_by.name", 0] },
+        },
+      },
+    ]);
+    // console.log(doc);
+    // let doc = await TicketComment.find({ ticket: req.params.id });
     if (!doc) {
       return next(
         new AppError(404, "fail", "No document found with that id"),
