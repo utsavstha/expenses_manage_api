4d0e3f009320a4abf8c7b3ebea5f603584cecd2e
Utsav Shrestha <utsavstha@outlook.com>
2022-02-18 08:46:59 +0545

    bug fixes

--- models/boardModel.js
+++ models/boardModel.js
@@ -2,11 +2,10 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 
 const boardSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: [true, "Please enter Board name"],
-    }
+  title: {
+    type: String,
+    required: [true, "Please enter Board name"],
+  },
 });
-
 const Board = mongoose.model("Board", boardSchema);
 module.exports = Board;
